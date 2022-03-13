79be0e421b0ebc820173af524aa92d017d4fd9f2
duyvanda <68499531+duyvanda@users.noreply.github.com>
2022-03-13 23:03:35 +0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -173,7 +173,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "157.230.37.174"
 ]
 
 # Default primary key field type
