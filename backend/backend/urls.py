9b2556ef7ca358ba87c715bf80998690e9048917
Duy Van <vanquangduy10@gmail.com>
2022-03-13 22:00:32 +0700

    changed

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,13 +4,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+ 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('api/products/', include('base.urls.product_urls')),
-    path('api/users/', include('base.urls.user_urls')),
-    path('api/orders/', include('base.urls.order_urls')),
+   path('admin/', admin.site.urls),
+   path('', TemplateView.as_view(template_name='index.html')),
+   path('api/products/', include('base.urls.product_urls')),
+   path('api/users/', include('base.urls.user_urls')),
+   path('api/orders/', include('base.urls.order_urls')),
 ]
-
+ 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
